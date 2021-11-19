c7cc35e40d0f270736cce764cd41c711aa9915b1
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2021-11-19 14:51:36 +0530

    added routes to search for nearest taxis

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .db import engine
+from .location import Location
 from .user import User
 from .taxi import Taxi
--- routers/taxis.py
+++ routers/taxis.py
@@ -1,7 +1,7 @@
 from typing import List
-from fastapi import APIRouter
 
-from models import engine, Taxi
+from fastapi import APIRouter
+from models import Location, Taxi, engine
 
 router = APIRouter(prefix="/taxis", tags=["Taxis"])
 
@@ -15,5 +15,37 @@ async def read_list(skip: int = 0, limit: int = 10):
 
 @router.post("/", response_model=Taxi)
 async def create(taxi: Taxi):
+    """Register new taxi"""
     await engine.save(taxi)
     return taxi
+
+
+@router.get("/near", response_model=List[Taxi])
+async def fetch_taxis_within_radius(
+    lat: float, long: float, distance: int = 1000, skip: int = 0, limit: int = 3
+):
+    """list taxis within the given distance (1km by default)"""
+    # todo fix the query
+    query = {
+        "location": [
+            ("$near", [lat, long]),
+            ("$maxDistance", distance),
+        ]
+    }
+    taxis = await engine.find(Taxi, query, skip=skip, limit=limit)
+    return taxis
+
+
+@router.get("/nearest", response_model=List[Taxi])
+async def fetch_nearest_taxis(
+    lat: float, long: float, distance: int = 1000, skip: int = 0, limit: int = 2
+):
+    """list nearest taxis (limited to 2 by default)"""
+    # todo fix the query
+    query = {
+        "location": [
+            ("$near", [lat, long]),
+        ]
+    }
+    taxis = await engine.find(Taxi, query, skip=skip, limit=limit)
+    return taxis
