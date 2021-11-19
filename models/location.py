9e44d2791fc71ea7dd3aac0123d5dad1f08e91e8
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2021-11-19 10:41:40 +0530

    fixed typing error

--- models/location.py
+++ models/location.py
@@ -4,4 +4,4 @@ from pydantic.types import conlist
 
 class Location(EmbeddedModel):
     type: str = "Point"
-    coordinates: conlist(float, min_items=2, max_items=2)
+    coordinates: conlist(float, min_items=2, max_items=2)  # type: ignore
--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 from odmantic import Model
-from pydantic.types import constr
+from pydantic import constr, EmailStr
 
 from .location import Location
 
 
 class User(Model):
-    name: constr(min_length=3, max_length=30)
+    name: constr(min_length=3, max_length=30)  # type: ignore
     location: Location
