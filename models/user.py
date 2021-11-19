0b826fce77021cf13c06da7500e90af506011f89
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2021-11-19 11:43:39 +0530

    added index on User.location key

--- models/user.py
+++ models/user.py
@@ -1,9 +1,16 @@
 from odmantic import Model
 from pydantic import constr, EmailStr
+from pymongo import GEOSPHERE
 
+from .db import engine
 from .location import Location
 
 
 class User(Model):
     name: constr(min_length=3, max_length=30)  # type: ignore
     location: Location
+
+
+# create indexes
+user_collection = engine.get_collection(User)
+user_collection.create_index([("location", GEOSPHERE)])
